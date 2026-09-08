d1074465f1664bcc87bd1b57533cbe9f5ed5eca9
bertiethorpe <bertie443@gmail.com>
2026-09-09 00:36:28 +0100

    MSL shaders, Build and bind GPU pipeline, GPU Triangle

--- src/main.c
+++ src/main.c
@@ -11,6 +11,10 @@ static const int height = 600;
 static SDL_Window *window = NULL;
 static SDL_GPUDevice *gpu_device = NULL;
 
+static SDL_GPUShader *vertex_shader = NULL;
+static SDL_GPUShader *fragment_shader = NULL;
+static SDL_GPUGraphicsPipeline *graphics_pipeline = NULL;
+
 void shutdown(void);
 
 // -------------------- Init / Shutdown --------------------
@@ -51,11 +55,133 @@ bool init(void) {
         return false;
     }
 
+    size_t vertex_shader_size = 0;
+    Uint8 *vertex_shader_code = SDL_LoadFile(
+        "shaders/triangle.vert.msl",
+        &vertex_shader_size
+    );
+
+    if (!vertex_shader_code) {
+        SDL_Log("Could not load vertex shader: %s", SDL_GetError());
+        shutdown();
+        return false;
+    }
+
+    size_t fragment_shader_size = 0;
+    Uint8 *fragment_shader_code = SDL_LoadFile(
+        "shaders/triangle.frag.msl",
+        &fragment_shader_size
+    );
+
+    if (!fragment_shader_code) {
+        SDL_Log("Could not load fragment shader: %s", SDL_GetError());
+        SDL_free(vertex_shader_code);
+        shutdown();
+        return false;
+    }
+
+    SDL_Log("Loaded vertex shader: %zu bytes", vertex_shader_size);
+    SDL_Log("Loaded fragment shader: %zu bytes", fragment_shader_size);
+
+    SDL_GPUShaderCreateInfo vertex_shader_info = {0};
+    vertex_shader_info.code = vertex_shader_code;
+    vertex_shader_info.code_size = vertex_shader_size;
+    vertex_shader_info.entrypoint = "vertex_main";
+    vertex_shader_info.format = SDL_GPU_SHADERFORMAT_MSL;
+    vertex_shader_info.stage = SDL_GPU_SHADERSTAGE_VERTEX;
+
+    vertex_shader = SDL_CreateGPUShader(
+        gpu_device,
+        &vertex_shader_info
+    );
+
+    if (!vertex_shader) {
+        SDL_Log("Could not create vertex shader: %s", SDL_GetError());
+        SDL_free(vertex_shader_code);
+        SDL_free(fragment_shader_code);
+        shutdown();
+        return false;
+    }
+
+    SDL_GPUShaderCreateInfo fragment_shader_info = {0};
+    fragment_shader_info.code = fragment_shader_code;
+    fragment_shader_info.code_size = fragment_shader_size;
+    fragment_shader_info.entrypoint = "fragment_main";
+    fragment_shader_info.format = SDL_GPU_SHADERFORMAT_MSL;
+    fragment_shader_info.stage = SDL_GPU_SHADERSTAGE_FRAGMENT;
+
+    fragment_shader = SDL_CreateGPUShader(
+        gpu_device,
+        &fragment_shader_info
+    );
+
+    if (!fragment_shader) {
+        SDL_Log("Could not create fragment shader: %s", SDL_GetError());
+        shutdown();
+        return false;
+    }
+
+    SDL_Log("Created both GPU shaders");
+
+    SDL_free(vertex_shader_code);
+    SDL_free(fragment_shader_code);
+
+    SDL_GPUColorTargetDescription color_target_description = {0};
+    color_target_description.format =
+        SDL_GetGPUSwapchainTextureFormat(gpu_device, window);
+
+    SDL_GPUGraphicsPipelineCreateInfo pipeline_info = {0};
+
+    pipeline_info.vertex_shader = vertex_shader;
+    pipeline_info.fragment_shader = fragment_shader;
+    pipeline_info.primitive_type = SDL_GPU_PRIMITIVETYPE_TRIANGLELIST;
+    pipeline_info.rasterizer_state.fill_mode = SDL_GPU_FILLMODE_FILL;
+
+    pipeline_info.target_info.num_color_targets = 1;
+    pipeline_info.target_info.color_target_descriptions =
+        &color_target_description;
+
+    graphics_pipeline = SDL_CreateGPUGraphicsPipeline(
+        gpu_device,
+        &pipeline_info
+    );
+
+    if (!graphics_pipeline) {
+        SDL_Log("Could not create graphics pipeline: %s", SDL_GetError());
+        shutdown();
+        return false;
+    }
+
+    SDL_Log("Created graphics pipeline");
+    
+    SDL_ReleaseGPUShader(gpu_device, vertex_shader);
+    SDL_ReleaseGPUShader(gpu_device, fragment_shader);
+
+    vertex_shader = NULL;
+    fragment_shader = NULL;
+
     SDL_SetWindowPosition(window, 50, 100);
     return true;
 }
 
 void shutdown(void) {
+    if (gpu_device) {
+        if (graphics_pipeline) {
+            SDL_ReleaseGPUGraphicsPipeline(
+                gpu_device,
+                graphics_pipeline
+            );
+        }
+
+        if (vertex_shader) {
+            SDL_ReleaseGPUShader(gpu_device, vertex_shader);
+        }
+
+        if (fragment_shader) {
+            SDL_ReleaseGPUShader(gpu_device, fragment_shader);
+        }
+    }
+
     if (gpu_device && window) {
         SDL_ReleaseWindowFromGPUDevice(gpu_device, window);
     }
@@ -124,6 +250,19 @@ bool render(void){
                 1,
                 NULL
             );
+
+        SDL_BindGPUGraphicsPipeline(
+            render_pass,
+            graphics_pipeline
+        );
+
+        SDL_DrawGPUPrimitives(
+            render_pass,
+            3,  // three vertices
+            1,  // one triangle instance
+            0,  // begin at vertex zero
+            0   // begin at instance zero
+        );
 
         SDL_EndGPURenderPass(render_pass);
     }
